c715e15e5d36e1fd7fd894f59e98af38b8d75512
ekistece <mr.tamuraa@gmail.com>
2017-02-25 11:13:37 +0100

    Create apod.js

---
+++ apod.js
@@ -0,0 +1,12 @@
+var url = "http://127.0.0.1:3000";
+var request = new XMLHttpRequest();
+request.open('GET', url);
+request.responseType = 'json';
+request.send()
+request.onload = function() {
+	var json = request.response;
+	document.getElementById('previewTitle').textContent = json['title'];
+	document.getElementById('previewInfo').textContent = json['info'];
+	document.getElementById('previewImg').src = json['img'];
+	document.getElementById('previewBody').textContent = json['body'];
+}
