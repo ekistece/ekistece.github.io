2a643092c247cb71b19e77768dc86020d7ff6e28
ekistece <mr.tamuraa@gmail.com>
2017-02-25 11:13:14 +0100

    Create facebook.js

---
+++ facebook.js
@@ -0,0 +1,64 @@
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      userLoggedIn();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      userLoggedOut();
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      userLoggedOut();
+    }
+  }
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+function userLoggedIn() {
+  btn = document.getElementById("loginButton");
+  btn.className = btn.className.replace("btn-success", "btn-danger");
+  document.getElementById("buttons_navbar").style.display = "block";
+  btn.textContent = "Log out";
+}
+
+function userLoggedOut() {
+  btn = document.getElementById("loginButton");
+   btn.className = btn.className.replace("btn-danger", "btn-success");
+   document.getElementById("buttons_navbar").style.display = "hide";
+   btn.textContent = "Log In";
+}
+
+function loginWithButton() {
+  FB.login(function(){
+  // Note: The call will only work if you accept the permission request
+  checkLoginState();
+}
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1321362891254036',
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+  });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
