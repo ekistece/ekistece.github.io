4cba455fd2b27146c06dcc5d61277a3fb459aa4a
ekistece <mr.tamuraa@gmail.com>
2017-02-25 15:38:43 +0100

    Update apod.js

--- apod.js
+++ apod.js
@@ -2,16 +2,19 @@ var url = "http://127.0.0.1:3000";
 var request = new XMLHttpRequest();
 request.open('GET', url);
 request.responseType = 'json';
-request.send();
-if (request.status == 200) {
-	request.onload = function() {
+request.onload = function() {
+	if (request.status == 200) {
 		var json = request.response;
 		document.getElementById('previewTitle').textContent = json['title'];
 		document.getElementById('previewInfo').textContent = json['info'];
 		document.getElementById('previewImg').src = json['img'];
 		document.getElementById('previewBody').textContent = json['body'];
-	};
+	}
+	else {
+		document.getElementById('previewTitle').textContent = "Couldn't connect to server...";
+	}
 }
-else {
+request.onerror = function() {
 	document.getElementById('previewTitle').textContent = "Couldn't connect to server...";
 }
+request.send();
