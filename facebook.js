aaafe78f4f4148b181b1f1d4f497736b21182a4a
ekistece <mr.tamuraa@gmail.com>
2017-02-26 13:06:44 +0100

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -25,9 +25,9 @@ function userLoggedIn() {
   btn.className = btn.className.replace("btn-success", "btn-danger");
   document.getElementById("buttons_navbar").style.display = "block";
   btn.textContent = "Log out";
-  btn.setAttribute("onclick", "logoutWithButton()")
+  btn.setAttribute("onclick", "logoutWithButton()");
   page_access_token = getPageAccessToken('TestPage');
-  console.log(page_access_token)
+  console.log(page_access_token);
 }
 
 function userLoggedOut() {
